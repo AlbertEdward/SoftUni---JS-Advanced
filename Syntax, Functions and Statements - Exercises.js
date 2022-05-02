629b299c2035e1632f122e9e9d2fd770742f6ff2
AlbertEdward <albert.khurshudyan96@gmail.com>
2022-05-02 23:27:46 +0300

    Solved some exercises

--- Syntax, Functions and Statements - Exercises.js
+++ Syntax, Functions and Statements - Exercises.js
@@ -65,3 +65,155 @@ function solve5(steps, footPrint, speedKmH) {
 solve5(4000, 0.60, 5);
 
 //06. Road Radar
+function solve6(inputSpeed, area) {
+    let motorway = 130;
+    let interstate = 90;
+    let city = 50;
+    let residential = 20;
+    let speed = Number(inputSpeed);
+    let status = ''
+
+    if(area == 'motorway'){
+        if(speed <= motorway){
+            console.log(`Driving ${speed} km/h in a ${motorway} zone`);
+        }
+        else if(speed < motorway + 21){
+            status = 'speeding';
+
+            console.log(`The speed is ${speed - motorway} km/h faster than the allowed speed of ${motorway} - ${status}`);
+        }
+        else if(speed > motorway + 20 && speed <= motorway + 40){
+            status = 'excessive speeding';
+
+            console.log(`The speed is ${speed - motorway} km/h faster than the allowed speed of ${motorway} - ${status}`);
+        }
+        else{
+            status = 'reckless driving';
+
+            console.log(`The speed is ${speed - motorway} km/h faster than the allowed speed of ${motorway} - ${status}`);
+        }
+        
+    }
+    else if(area == 'interstate'){
+        if(speed <= interstate){
+            console.log(`Driving ${speed} km/h in a ${interstate} zone`);
+        }
+        else if(speed < interstate + 21){
+            status = 'speeding';
+
+            console.log(`The speed is ${speed - interstate} km/h faster than the allowed speed of ${interstate} - ${status}`);
+        }
+        else if(speed > interstate + 20 && speed <= interstate + 40){
+            status = 'excessive speeding';
+
+            console.log(`The speed is ${speed - interstate} km/h faster than the allowed speed of ${interstate} - ${status}`);
+        }
+        else{
+            status = 'reckless driving';
+
+            console.log(`The speed is ${speed - interstate} km/h faster than the allowed speed of ${interstate} - ${status}`);
+        }
+    }
+    else if(area == 'city'){
+        if(speed <= city){
+            console.log(`Driving ${speed} km/h in a ${city} zone`);
+        }
+        else if(speed < city + 20){
+            status = 'speeding';
+
+            console.log(`The speed is ${speed - city} km/h faster than the allowed speed of ${city} - ${status}`);
+        }
+        else if(speed > city + 20 && speed <= city + 40){
+            status = 'excessive speeding';
+
+            console.log(`The speed is ${speed - city} km/h faster than the allowed speed of ${city} - ${status}`);
+        }
+        else{
+            status = 'reckless driving';
+
+            console.log(`The speed is ${speed - city} km/h faster than the allowed speed of ${city} - ${status}`);
+        }
+    }
+    else if(area == 'residential'){
+        if(speed <= residential){
+            console.log(`Driving ${speed} km/h in a ${residential} zone`);
+        }
+        else if(speed < residential + 20){
+            status = 'speeding';
+
+            console.log(`The speed is ${speed - residential} km/h faster than the allowed speed of ${residential} - ${status}`);
+        }
+        else if(speed > residential + 20 && speed <= residential + 40){
+            status = 'excessive speeding';
+
+            console.log(`The speed is ${speed - residential} km/h faster than the allowed speed of ${residential} - ${status}`);
+        }
+        else{
+            status = 'reckless driving';
+
+            console.log(`The speed is ${speed - residential} km/h faster than the allowed speed of ${residential} - ${status}`);
+        }
+    }
+}
+solve6(20, 'residential');
+
+//07. Cooking by Numbers
+function solve7(number, commands0, commands1, commands2, commands3, commands4) {
+    let result = Number(number)
+    let commands = new Array(commands0, commands1, commands2, commands3, commands4);
+
+    for(let i = 0; i < commands.length; i++){
+        if(commands[i] == 'chop'){
+            result /= 2;
+            console.log(result);
+        }
+        else if(commands[i] == 'dice'){
+            result = Math.sqrt(result);
+            console.log(result);
+        }
+        else if(commands[i] == 'spice'){
+            result += 1;
+            console.log(result);
+        }
+        else if(commands[i] == 'bake'){
+            result *= 3;
+            console.log(result);
+        }
+        else if(commands[i] == 'fillet'){
+            result -= result * 0.20;
+            console.log(result);
+        }
+    }
+}
+solve7(9, 'dice', 'spice', 'chop', 'bake', 'fillet');
+
+//08. Validity Checker
+function solve8(x1, y1, x2, y2) {
+    let comparisons1 = Math.sqrt(Math.pow(x1 - y1, 2) + Math.pow(0 - 0, 2));
+    let comparisons2 = Math.sqrt(Math.pow(x2 - y2, 2) + Math.pow(0 - 0, 2));
+    let comparisons3 = Math.sqrt(Math.pow(x1 - y1, 2) + Math.pow(x2 - y2, 2));
+
+    if(Number.isInteger(comparisons1)){
+        console.log(`{${x1}, ${y1}} to {0, 0} is 'valid'`);
+    }
+    else{
+        console.log(`{${x1}, ${y1}} to {0, 0} is 'invalid'`);
+    }
+
+    if(Number.isInteger(comparisons2)){
+        console.log(`{${x2}, ${y2}} to {0, 0} is 'valid'`);
+    }
+    else{
+        console.log(`{${x2}, ${y2}} to {0, 0} is 'invalid'`);
+    }
+    
+    if(Number.isInteger(comparisons3)){
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is 'valid'`);
+    }
+    else{
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is 'invalid'`);
+    }
+
+    
+}
+solve8(2, 1, 1, 1);
